c1b5803c14664b0875e5fa9e317384617783a803
Sarah Ashraf <sarah.ashraf234@gmail.com>
2017-12-20 18:32:35 +0200

    Thread class completed

--- WiresharkWannabe/WiresharkWannabe/src/wiresharkwannabe/Thread.java
+++ WiresharkWannabe/WiresharkWannabe/src/wiresharkwannabe/Thread.java
@@ -52,39 +52,39 @@ import java.net.URL;
 // For formatting mac & ip output
 import org.jnetpcap.packet.format.FormatUtils;
 
-
 /**
  *
  * @author Farida Abouish
  */
-public class Thread extends Service{
+public class Thread extends Service {
+
     public static Ip4 ip = new Ip4();
     public static Ethernet eth = new Ethernet();
     public static Tcp tcp = new Tcp();
     public static Udp udp = new Udp();
-   public static Arp arp = new Arp();
-   public static Http http = new Http(); 
-   
+    public static Arp arp = new Arp();
+    public static Http http = new Http();
+
     public static List<PcapIf> alldevs = new ArrayList<PcapIf>();
     public static StringBuilder errbuf = new StringBuilder(); // For any error msgs  
     public static Pcap pcap;
-   // public static PcapPacketHandler<String> jpacketHandler ;
+    // public static PcapPacketHandler<String> jpacketHandler ;
     public static String src;
     public static String dest;
     public static String protocol;
-    public static  Date header;
+    public static Date header;
     public static int caplen;
     public static Info info;
-    public static ArrayList <Info> packetInfo = new ArrayList();
+    public static ArrayList<Info> packetInfo = new ArrayList();
     public static int counter = 0;
     static int r = Pcap.findAllDevs(alldevs, errbuf);
-     //PcapIf  device = alldevs.get(0);
-    
-    public Thread (){
-    int snaplen = 64 * 1024;           // Capture all packets, no trucation  
+    //PcapIf  device = alldevs.get(0);
+
+    public Thread() {
+        int snaplen = 64 * 1024;           // Capture all packets, no trucation  
         int flags = Pcap.MODE_PROMISCUOUS; // capture all packets  
         int timeout = 10 * 1000;           // 10 seconds in millis  
-        pcap= Pcap.openLive(WiresharkWannabe.device.getName(), snaplen, flags, timeout, errbuf);
+        pcap = Pcap.openLive(WiresharkWannabe.device.getName(), snaplen, flags, timeout, errbuf);
 
         if (pcap == null) {
             System.err.printf("Error while opening device for capture: "
@@ -92,69 +92,102 @@ public class Thread extends Service{
             return;
         }
     }
-    
-   PcapPacketHandler<String> jpacketHandler = new PcapPacketHandler<String>() {
 
-            public void nextPacket(PcapPacket packet, String user) {
-              //  String hexdump = packet.toHexdump(packet.size(), false, false, true);
-                //  System.out.println(hexdump);
-                System.out.printf("Received packet at %s caplen=%-4d len=%-4d %s\n",
-                        new Date(packet.getCaptureHeader().timestampInMillis()),
-                        packet.getCaptureHeader().caplen(), // Length actually captured  
-                        packet.getCaptureHeader().wirelen(), // Original length   
-                        user // User supplied object  
+    PcapPacketHandler<String> jpacketHandler = new PcapPacketHandler<String>() {
 
-                );
-                if (packet.hasHeader(ip)) {
+        public void nextPacket(PcapPacket packet, String user) {
+            //  String hexdump = packet.toHexdump(packet.size(), false, false, true);
+            //  System.out.println(hexdump);
+            System.out.printf("Received packet at %s caplen=%-4d len=%-4d %s\n",
+                    new Date(packet.getCaptureHeader().timestampInMillis()),
+                    packet.getCaptureHeader().caplen(), // Length actually captured  
+                    packet.getCaptureHeader().wirelen(), // Original length   
+                    user // User supplied object  
+
+            );
+            if (packet.hasHeader(ip)) {
 //                    if (FormatUtils.ip(ip.source()) != FormatUtils.ip(myinet)
 //                            && FormatUtils.ip(ip.destination()) != FormatUtils.ip(myinet)) {
 //                        System.out.println();
-                     System.out.println("IP type:\t" + ip.typeEnum());
+                System.out.println("IP type:\t" + ip.typeEnum());
 //                        System.out.println("IP src:\t-\t" + FormatUtils.ip(ip.source()));
 //                  System.out.println("IP dst:\t-\t" + FormatUtils.ip(ip.destination()));
 //                        readdata = true;
-                        src = FormatUtils.ip(ip.source());
-                        dest = FormatUtils.ip(ip.destination());
-                        protocol = "IP";
-                       
-                //}
-                }
-                if (packet.hasHeader(eth))
-//                        && readdata == true)
-                        {
-                    System.out.println("Ethernet type:\t" + eth.typeEnum());
-                    System.out.println("Ethernet src:\t" + FormatUtils.mac(eth.source()));
-                    System.out.println("Ethernet dst:\t" + FormatUtils.mac(eth.destination()));
-                     src = FormatUtils.mac(eth.source());
-                        dest = FormatUtils.mac(eth.destination());
-                        protocol = "Ethernet";
-                         
-                }
-                if(packet.hasHeader(http)){
                 src = FormatUtils.ip(ip.source());
                 dest = FormatUtils.ip(ip.destination());
-                        protocol = "HTTP";
-                }
-               
+                protocol = "IP";
 
-           header = new Date(packet.getCaptureHeader().timestampInMillis());
-           String time = String.valueOf(header);
-           caplen = packet.getCaptureHeader().caplen();// Length actually captured  
-           String leng = String.valueOf(caplen);
-           String counterStr = String.valueOf(counter);
-           info = new Info(counterStr,time,src,dest,protocol,leng,packet);
-           WiresharkWannabe.information.add(info);
-           counter++;
-                        
-            
+                //}
+            }
+            if (packet.hasHeader(eth)) //                        && readdata == true)
+            {
+                System.out.println("Ethernet type:\t" + eth.typeEnum());
+                System.out.println("Ethernet src:\t" + FormatUtils.mac(eth.source()));
+                System.out.println("Ethernet dst:\t" + FormatUtils.mac(eth.destination()));
+                src = FormatUtils.mac(eth.source());
+                dest = FormatUtils.mac(eth.destination());
+                protocol = "Ethernet";
+
+            }
+            if (packet.hasHeader(http)) {
+                src = FormatUtils.ip(ip.source());
+                dest = FormatUtils.ip(ip.destination());
+                protocol = "HTTP";
             }
 
-        };
+            if (packet.hasHeader(arp)) {
+                src = FormatUtils.ip(ip.source());
+                dest = FormatUtils.ip(ip.destination());
+                protocol = "ARP";
+            }
+            if (packet.hasHeader(tcp)) //                        && readdata == true)
+            {
+                System.out.println("TCP src port:\t" + tcp.source());
+                System.out.println("TCP dst port:\t" + tcp.destination());
+                src = String.valueOf(tcp.source());
+                dest = String.valueOf(tcp.destination());
+                protocol = "TCP";
+            } else if (packet.hasHeader(udp)) //                        && readdata == true)
+            {
+                System.out.println("UDP src port:\t" + udp.source());
+                System.out.println("UDP dst port:\t" + udp.destination());
+                src = String.valueOf(udp.source());
+                dest = String.valueOf(udp.destination());
+                protocol = "UDP";
+
+            }
+            header = new Date(packet.getCaptureHeader().timestampInMillis());
+            String time = String.valueOf(header);
+            caplen = packet.getCaptureHeader().caplen();// Length actually captured  
+            String leng = String.valueOf(caplen);
+            String counterStr = String.valueOf(counter);
+            info = new Info(counterStr, time, src, dest, protocol, leng, packet);
+            WiresharkWannabe.information.add(info);
+            counter++;
+
+        }
+
+    };
 
     @Override
     protected Task createTask() {
-       
+
+        return new Task() {
+            @Override
+            protected Object call() throws Exception {
+                while (true) {
+                    if (isCancelled()) {
+                        break;
+                    }
+
+                    pcap.loop(1, jpacketHandler, "");
+                }
+                return null;
+            }
+        };
     }
+
+}
 
     
     
