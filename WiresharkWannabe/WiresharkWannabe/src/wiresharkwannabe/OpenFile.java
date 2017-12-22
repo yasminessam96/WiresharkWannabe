0bee470d250853323347563d4a98f4a693fc471c
farida-abouish <32582370+farida-abouish@users.noreply.github.com>
2017-12-22 14:37:56 +0200

    trials

--- WiresharkWannabe/WiresharkWannabe/src/wiresharkwannabe/OpenFile.java
+++ WiresharkWannabe/WiresharkWannabe/src/wiresharkwannabe/OpenFile.java
@@ -23,13 +23,12 @@ import org.jnetpcap.protocol.tcpip.Http;
 import org.jnetpcap.protocol.tcpip.Tcp;
 import org.jnetpcap.protocol.tcpip.Udp;
 
-
 /**
  *
  * @author Yasmin
  */
 public class OpenFile extends Service {
-  
+
     public static String src;
     public static String dest;
     public static String prot;
@@ -39,7 +38,7 @@ public class OpenFile extends Service {
     public static ArrayList<Info> packetInfo = new ArrayList();
     public static int counter = 0;
     final StringBuilder errbuf = new StringBuilder();
-     public static Ip4 ip = new Ip4();
+    public static Ip4 ip = new Ip4();
     public static Ethernet eth = new Ethernet();
     public static Tcp tcp = new Tcp();
     public static Udp udp = new Udp();
@@ -47,81 +46,80 @@ public class OpenFile extends Service {
     public static Http http = new Http();
     PcapPacketHandler<String> jpacketHandler;
     String fname;
-     Pcap pcap2;
-    public OpenFile(String fname){
+    Pcap pcap2;
+
+    public OpenFile(String fname) {
         this.fname = fname;
-    pcap2 = Pcap.openOffline(this.fname, errbuf);
-     System.out.print(errbuf);
-       System.out.print(fname);
-   this.jpacketHandler = new PcapPacketHandler<String>() {
+        pcap2 = Pcap.openOffline(this.fname, errbuf);
+        System.out.print(errbuf);
+        System.out.print(fname);
+        this.jpacketHandler = new PcapPacketHandler<String>() {
 
-                public void nextPacket(PcapPacket packet, String user) {
+            public void nextPacket(PcapPacket packet, String user) {
 
-            System.out.printf("Received packet at %s caplen=%-4d len=%-4d %s\n",
-                    new Date(packet.getCaptureHeader().timestampInMillis()),
-                    packet.getCaptureHeader().caplen(), // Length actually captured  
-                    packet.getCaptureHeader().wirelen(), // Original length   
-                    user // User supplied object  
+                System.out.printf("Received packet at %s caplen=%-4d len=%-4d %s\n",
+                        new Date(packet.getCaptureHeader().timestampInMillis()),
+                        packet.getCaptureHeader().caplen(), // Length actually captured  
+                        packet.getCaptureHeader().wirelen(), // Original length   
+                        user // User supplied object  
 
-            );
-            if (packet.hasHeader(ip)) {
+                );
+                if (packet.hasHeader(ip)) {
 
-                src = FormatUtils.ip(ip.source());
-                dest = FormatUtils.ip(ip.destination());
-                prot = "IP";
-System.out.println(prot);
-                //}
-            }
-            if (packet.hasHeader(eth)) {
-                src = FormatUtils.mac(eth.source());
-                dest = FormatUtils.mac(eth.destination());
-                prot = "Ethernet";
-System.out.println(prot);
-            }
-            if (packet.hasHeader(http)) {
-                src = FormatUtils.ip(ip.source());
-                dest = FormatUtils.ip(ip.destination());
-                prot = "HTTP";
-                System.out.println(prot);
-            }
-
-            if (packet.hasHeader(arp)) {
-                src = FormatUtils.ip(ip.source());
-                dest = FormatUtils.ip(ip.destination());
-                prot = "ARP";
-            }
-            if (packet.hasHeader(tcp)) {
-                System.out.println("TCP src port:\t" + tcp.source());
-                System.out.println("TCP dst port:\t" + tcp.destination());
-                src = String.valueOf(tcp.source());
-                dest = String.valueOf(tcp.destination());
-                prot = "TCP";
-            } else if (packet.hasHeader(udp)) {
-                System.out.println("UDP src port:\t" + udp.source());
-                System.out.println("UDP dst port:\t" + udp.destination());
-                src = String.valueOf(udp.source());
-                dest = String.valueOf(udp.destination());
-                prot = "UDP";
-                
-
-            }
-            header = new Date(packet.getCaptureHeader().timestampInMillis());
-            String time = String.valueOf(header);
-            caplen = packet.getCaptureHeader().caplen();// Length actually captured  
-            String leng = String.valueOf(caplen);
-            String counterStr = String.valueOf(counter);
-            info = new Info(counterStr, time, src, dest, prot, leng, packet);
-            WiresharkWannabe.information.add(info);
-            System.out.println(prot);
-              System.out.println(counter);
-            counter++;
-
-        }
- 
-    };
-              
-                 
+                    src = FormatUtils.ip(ip.source());
+                    dest = FormatUtils.ip(ip.destination());
+                    prot = "IP";
+                    System.out.println(prot);
+                    //}
                 }
+                if (packet.hasHeader(eth)) {
+                    src = FormatUtils.mac(eth.source());
+                    dest = FormatUtils.mac(eth.destination());
+                    prot = "Ethernet";
+                    System.out.println(prot);
+                }
+                if (packet.hasHeader(http)) {
+                    src = FormatUtils.ip(ip.source());
+                    dest = FormatUtils.ip(ip.destination());
+                    prot = "HTTP";
+                    System.out.println(prot);
+                }
+
+                if (packet.hasHeader(arp)) {
+                    src = FormatUtils.ip(ip.source());
+                    dest = FormatUtils.ip(ip.destination());
+                    prot = "ARP";
+                }
+                if (packet.hasHeader(tcp)) {
+                    System.out.println("TCP src port:\t" + tcp.source());
+                    System.out.println("TCP dst port:\t" + tcp.destination());
+                    src = String.valueOf(tcp.source());
+                    dest = String.valueOf(tcp.destination());
+                    prot = "TCP";
+                } else if (packet.hasHeader(udp)) {
+                    System.out.println("UDP src port:\t" + udp.source());
+                    System.out.println("UDP dst port:\t" + udp.destination());
+                    src = String.valueOf(udp.source());
+                    dest = String.valueOf(udp.destination());
+                    prot = "UDP";
+
+                }
+                header = new Date(packet.getCaptureHeader().timestampInMillis());
+                String time = String.valueOf(header);
+                caplen = packet.getCaptureHeader().caplen();// Length actually captured  
+                String leng = String.valueOf(caplen);
+                String counterStr = String.valueOf(counter);
+                info = new Info(counterStr, time, src, dest, prot, leng, packet);
+                WiresharkWannabe.information.add(info);
+                System.out.println(prot);
+                System.out.println(counter);
+                counter++;
+
+            }
+
+        };
+
+    }
 
     @Override
     protected Task createTask() {
@@ -140,6 +138,4 @@ System.out.println(prot);
             }
         };
     }
-    }
- 
-
+}
